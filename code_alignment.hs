514fe0cfd8310f3a3c04b2f28fc8760b55eb5694
Thomas Swann <tkswann@mac.com>
2016-11-14 18:46:23 -0600

    some examples of code alignment

---
+++ code_alignment.hs
@@ -0,0 +1,6 @@
+journeycost :: Float -> Float -> Float
+journeycost miles fuelcostperlitre =
+ let milespergallon = 35
+     litrespergallon = 4.55
+     gallons = miles/milespergallon
+ in (gallons*litrespergallon*fuelcostperlitre)
